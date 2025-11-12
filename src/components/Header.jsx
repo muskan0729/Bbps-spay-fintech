8261bc685888f3201b64da53121457f7dc0a8469
unknown <intern@spay.live>
2025-11-12 17:57:59 +0530

    designed user sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,75 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWallet, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import avatarImg from "../images/placeholder.jpeg";
-import spayImg from "../images/placeholder.jpeg";
+// import spayImg from "../images/placeholder.jpeg";
+import logo from "../images/Spaylogo.jpg"
+import profilePic from "../images/avatars/1.png";
+
 
 import { NotificationBell } from "./NotificationBell";
-
-// const mockNotifications = [
-// 	{ id: 1, message: "New report generated successfully." },
-// 	{ id: 2, message: "Complaint #101 assigned to you." },
-// 	{ id: 3, message: "System maintenance scheduled for 2 AM." },
-// ];
+import { ProfileSidebar } from "./ProfileSidebar";
 
 export const Header = () => {
-	return (
-		<header className="sticky top-0 py-1 z-50 bg-white shadow-xl border-b border-blue-100">
-			<div className="mx-auto px-3">
-				<div className="flex justify-between items-center h-16">
-					<div className="relative h-15 flex items-center justify-center">
-						<img
-							src={spayImg}
-							alt="User avatar"
-							className="w-full h-full object-cover"
-						/>
-					</div>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-					{/* Right side - Avatar */}
-					<div className="flex items-center space-x-3">
-						<div className="text-amber-900">
-							<FontAwesomeIcon icon={faWallet} />
-						</div>
-						<span className="text-gray-700 font-bold">
-							₹ 5,000/-
-						</span>
-						<NotificationBell/>
-						<div className="relative w-16 h-16 rounded-full flex items-center justify-center overflow-hidden">
-							<img
-								src={avatarImg}
-								alt="User avatar"
-								className="w-full h-full object-cover rounded-full"
-							/>
-						</div>
-						<button className="text-blue-500 hover:text-blue-700 font-bold">
-							<FontAwesomeIcon icon={faCaretDown} />
-						</button>
-					</div>
-				</div>
+  return (
+    <header className="sticky top-0 py-1 z-50 bg-white shadow-xl border-b border-blue-100">
+      <div className="mx-auto px-3">
+        <div className="flex justify-between items-center h-16">
+          {/* Left - Logo */}
+          <div className="relative h-15 flex items-center justify-center">
+            <img
+            //   src={spayImg}
+              src={logo}
+              alt="User avatar"
+              className="w-full h-full object-cover "
+            />
+          </div>
+
+          {/* Right side - Wallet, Notifications, Avatar, Button */}
+          <div className="flex items-center space-x-3">
+            <div className="text-amber-900">
+              <FontAwesomeIcon icon={faWallet} />
+            </div>
+            <span className="text-gray-700 font-bold">₹ 5,000/-</span>
+            <NotificationBell />
+
+			<button
+			onClick={() => setIsSidebarOpen(true)}
+			className="flex items-center space-x-2 cursor-pointer focus:outline-none">
+			<div className="relative w-12 h-12 rounded-full overflow-hidden border-2 border-blue-100 hover:shadow-md transition">
+				<img
+				src={profilePic}
+				alt="User avatar"
+				className="w-full h-full object-cover rounded-full"
+				/>
 			</div>
-		</header>
+			<FontAwesomeIcon
+				icon={faCaretDown}
+				className="text-blue-500 hover:text-blue-700"
+			/>
+			</button>
+			
+          </div>
+        </div>
+      </div>
+
+
+		{/* Overlay behind sidebar */}
+		{/* {isSidebarOpen && (
+		<div
+			className="fixed top-0 left-0 h-full w-[100%] bg-black bg-opacity-40 z-40"
+			onClick={() => setIsSidebarOpen(false)}
+		></div>
+		)} */}
+
+		{/* Sidebar Component */}
+		<ProfileSidebar
+			isOpen={isSidebarOpen}
+			onClose={() => setIsSidebarOpen(false)}
+		/>
+
+	 	</header>
 	);
 };
